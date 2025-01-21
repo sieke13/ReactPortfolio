02cba22830f5b353fb9df1e672fcd03817d7dde9
sieke13 <gera.ave2@gmail.com>
2025-01-20 18:20:40 -0600

    yes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header-content">
-        <img src="public\assets\dududancing.gif" alt="Logo" className="logo" />
+        <img src="assets\dududancing.gif" alt="Logo" className="logo" />
         <h1>Gerardo Avendano Portfolio</h1>
         <Navigation />
       </div>
